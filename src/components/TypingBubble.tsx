9a480d30c3b809d401f91c98dbbefa5c5047bd4d
reedycat <wpsells@gmail.com>
2024-02-21 12:24:42 +0200

    fix 7

--- src/components/TypingBubble.tsx
+++ src/components/TypingBubble.tsx
@@ -3,17 +3,11 @@ type TProps = {
 };
 
 export const TypingBubble = (props: TProps) => {
-  const bubleStyle = props.typingBubbleColor
-    ? {
-        'background-color': props.typingBubbleColor,
-      }
-    : '';
-
   return (
     <div class="flex items-center">
-      <div class="w-2 h-2 mr-1 rounded-full bubble1" style={bubleStyle} />
-      <div class="w-2 h-2 mr-1 rounded-full bubble2" style={bubleStyle} />
-      <div class="w-2 h-2 rounded-full bubble3" style={bubleStyle} />
+      <div class="w-2 h-2 mr-1 rounded-full bubble1" style={props.typingBubbleColor ? { 'background-color': props.typingBubbleColor } : ''} />
+      <div class="w-2 h-2 mr-1 rounded-full bubble2" style={props.typingBubbleColor ? { 'background-color': props.typingBubbleColor } : ''} />
+      <div class="w-2 h-2 rounded-full bubble3" style={props.typingBubbleColor ? { 'background-color': props.typingBubbleColor } : ''} />
     </div>
   );
 };
